96a80e06afdbec685ddf514c4af8c96e8c2030ca
J <09.chen@gmail.com>
2024-03-02 15:05:55 -0800

    Added Horoscope API

--- client/src/pages/Horoscopes.js
+++ client/src/pages/Horoscopes.js
@@ -1,14 +1,38 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import HoroscopeCard from "../components/HoroscopeCard";
 
 import "../styles/Horoscopes.css";
 
 const Horoscopes = () => {
   const [zodiacSign, setZodiacSign] = useState("");
+  const [dailyHoroscope, setDailyHoroscope] = useState([]);
+  const [dateHoroscope, setDateHoroscope] = useState([]);
 
-  const getSign = (sign) => {
-    setZodiacSign(sign);
-  };
+  const url = `https://horoscope-astrology.p.rapidapi.com/horoscope?day=today&sunsign=capricorn`;
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '396d65978cmsh536e29951248595p199c25jsn69e49935a220',
+		'X-RapidAPI-Host': 'horoscope-astrology.p.rapidapi.com'
+	}
+};
+
+const getHoroscope = async () => {
+try {
+	const response = await fetch(url, options);
+	const result = await response.json(); 
+	console.log(result.date);
+  setDateHoroscope(result.date);
+  setDailyHoroscope(result.horoscope);
+} catch (error) {
+	console.error(error);
+}
+}
+
+useEffect(() => {
+    getHoroscope();
+}, []); 
+
 
   return (
     <>
@@ -16,7 +40,7 @@ const Horoscopes = () => {
     <div className="horoscope-section mt-5 mb-5">
       <h2>Select Your Horoscope</h2>
       <div className="horoscope-select">
-        <HoroscopeCard title="Capricorn" description="December 22 - January 19" zodiacSign={zodiacSign} getSign={getSign}/>
+        <HoroscopeCard title="Capricorn" description="December 22 - January 19" getSign={getHoroscope}/>
         <HoroscopeCard title="Aquarius" description="January 20 - February 18"/>
         <HoroscopeCard title="Pisces" description="February 19 - March 20"/>
         <HoroscopeCard title="Aries" description="March 21 - April 19"/>
@@ -28,6 +52,10 @@ const Horoscopes = () => {
         <HoroscopeCard title="Libra" description="September 23 - October 22"/>
         <HoroscopeCard title="Scorpio" description="October 23 - November 21"/>
         <HoroscopeCard title="Sagittarius" description="November 22 - December 21"/>
+      </div>
+      <div>
+        Date: {dateHoroscope}
+        Horoscope:{dailyHoroscope}
       </div>
     </div>
   )
